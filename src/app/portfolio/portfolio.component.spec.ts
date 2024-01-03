b36c730b83e48d6ea73fb74a602254b586981ac5
Barna Zsombor Klara <zsomboro@gmail.com>
2024-01-03 12:07:01 +0100

    fix tests

--- src/app/portfolio/portfolio.component.spec.ts
+++ src/app/portfolio/portfolio.component.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PortfolioComponent } from './portfolio.component';
-import { HttpClientModule } from '@angular/common/http';
+import { PortfolioService } from './portfolio.service';
+import { Observable, of } from 'rxjs';
+import { Portfolio } from "./portfolio";
+
+let portfolioServiceStub:  Partial<PortfolioService>;
 
 describe('Portfolio List Component', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [PortfolioComponent, HttpClientModule]
-  }));
+
+  let component: PortfolioComponent;
+  let fixture: ComponentFixture<PortfolioComponent>;
+
+  beforeEach(() => {
+    const portfolio =  {
+      name: "My Portfolio",
+      id : 1,
+      constituents : [{instrument: "IBM", number: 20}, {instrument: "SP500", number: 80}]
+    }
+    portfolioServiceStub = {
+      getPortfolio() : Observable<Portfolio> {  return of(portfolio);}
+    };
+    TestBed.configureTestingModule({
+      imports: [PortfolioComponent],
+      providers: [{ provide: PortfolioService, useValue: portfolioServiceStub }],
+    }).compileComponents();
+    fixture = TestBed.createComponent(PortfolioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+});
 
   it('should create the portfolio list', () => {
-    const fixture = TestBed.createComponent(PortfolioComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have portfolio default name as 'EMPTY'`, () => {
@@ -20,8 +40,6 @@ describe('Portfolio List Component', () => {
   });
 
   it('should render portfolio name', () => {
-    const fixture = TestBed.createComponent(PortfolioComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h2')?.textContent).toContain('My Portfolio');
   });
